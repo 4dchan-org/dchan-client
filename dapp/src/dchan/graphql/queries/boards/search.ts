ab309cb1349f506ef639b1180c11d463c3496bfd
op <op@dchan.network>
2021-09-04 18:54:03 +0200

    Fix boards

--- dapp/src/dchan/graphql/queries/boards/search.ts
+++ dapp/src/dchan/graphql/queries/boards/search.ts
@@ -1,7 +1,10 @@
 import { gql } from "apollo-boost";
+import BOARD_FRAGMENT from "dchan/graphql/fragments/board";
 
 const BOARDS_SEARCH = gql`
-  query BoardsSearch($searchName: String, $searchTitle: String) {
+  ${BOARD_FRAGMENT}
+
+  query BoardsSearch($searchName: String!, $searchTitle: String!) {
     searchByTitle: boardSearch(text: $searchTitle) {
       ...Board
     }
