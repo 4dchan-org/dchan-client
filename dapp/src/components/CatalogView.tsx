da6403f031cdff444b8e6569b213796af8daa9f5
Frawst <fraw@fraw.st>
2021-09-15 01:04:45 +0100

    Catalog threads focus on hover instead of click

--- dapp/src/components/CatalogView.tsx
+++ dapp/src/components/CatalogView.tsx
@@ -1,7 +1,5 @@
 import { Thread } from "dchan";
 import CatalogThread from "./catalog/CatalogThread";
-import { RefObject, createRef, useEffect } from "react";
-import usePubSub from "hooks/usePubSub";
 
 export default function CatalogView({
   threads,
@@ -12,39 +10,16 @@ export default function CatalogView({
   block?: number,
   showBoard?: boolean
 }) {
-  let threadRefs: RefObject<HTMLElement>[] = [];
-  let threadElements = [];
-
-  for (const thread of threads.filter(t => !!t && !!t.board)) {
-    const threadRef = createRef<HTMLElement>();
-    threadRefs.push(threadRef);
-    threadElements.push(<CatalogThread
-      ref={threadRef}
-      thread={thread}
-      key={thread.id}
-      block={block}
-      showBoard={showBoard}
-    />);
-  }
-
-  const { publish } = usePubSub();
-
-  useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (threadRefs.every(ref => ref && ref.current && !ref.current.contains(event.target))) {
-        publish("THREAD_FOCUS", "");
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  });
-
   return (
     <div className="grid grid-template-columns-ram-150px place-items-start font-size-090rem px-4 sm:px-8">
-      {threadElements}
+      {threads.filter(t => !!t && !!t.board).map(thread => (
+        <CatalogThread
+          thread={thread}
+          key={thread.id}
+          block={block}
+          showBoard={showBoard}
+        />
+      ))}
     </div>
   );
 }
