7c559279162968aa5d3524f46ea4d045e3c787e0
op <op@dchan.network>
2021-09-16 02:39:25 +0200

    Fixes

--- dapp/src/components/post/PostBody.tsx
+++ dapp/src/components/post/PostBody.tsx
@@ -1,5 +1,5 @@
 import { Post } from 'dchan';
-import { BACKLINK_HTML_SAFE_REGEX, EXTERNAL_LINK_REGEX, IPFS_HASH_REGEX, NEWLINE_REGEX, REF_BOARD_HTML_SAFE_REGEX, REF_HTML_SAFE_REGEX, REF_POST_HTML_SAFE_REGEX, SPOILER_REGEX, TEXT_QUOTES_HTML_SAFE_REGEX } from 'dchan/regexps';
+import { BACKLINK_HTML_SAFE_REGEX, EXTERNAL_LINK_REGEX, NEWLINE_REGEX, REF_BOARD_HTML_SAFE_REGEX, REF_HTML_SAFE_REGEX, REF_POST_HTML_SAFE_REGEX, SPOILER_REGEX, TEXT_QUOTES_HTML_SAFE_REGEX } from 'dchan/regexps';
 import { Router } from 'router';
 import sanitizeHtml from 'sanitize-html';
 
@@ -15,7 +15,6 @@ export default function PostBody({post, style = {}}: {style?: any, post: Post}) 
     .replace(BACKLINK_HTML_SAFE_REGEX, `<a class="text-blue-600 visited:text-purple-600 hover:text-blue-500" href="#${baseUrl}$1">&gt;&gt;$1$2</a>`) // Post backlinks // @HACK
     .replace(SPOILER_REGEX, `<span class="dchan-post-spoiler">$1</span>`) // Spoilers
     .replace(EXTERNAL_LINK_REGEX, `<a class="text-blue-600 visited:text-purple-600 hover:text-blue-500" href="$1" target="_blank" rel="noreferrer">$1</a>`) // Links
-    .replace(IPFS_HASH_REGEX, `<details class="inline"><summary><a class="text-blue-600 visited:text-purple-600 hover:text-blue-500" href="//ipfs.io/ipfs/$1" target="_blank" rel="noreferrer">$1</a></summary><img src="//ipfs.io/ipfs/$1"/></details>`) // IPFS image embed
     .replace(NEWLINE_REGEX, `<br>`)
     
     return (
