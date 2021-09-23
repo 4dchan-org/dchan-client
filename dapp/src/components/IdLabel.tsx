9ace8738d1b20df494e1b725af3c1a749b769fa3
Frawst <fraw@fraw.st>
2021-09-23 12:36:42 +0100

    Change IdLabel to use luminosity for font

--- dapp/src/components/IdLabel.tsx
+++ dapp/src/components/IdLabel.tsx
@@ -1,5 +1,14 @@
 import { backgroundColorAddress, shortenAddress } from "dchan";
 
+function luma(rgb: string): number {
+  const rgbNum = parseInt(rgb.slice(1), 16);
+  const colors = [
+    (rgbNum >> 16) & 0xFF,
+    (rgbNum >> 8) & 0xFF,
+    rgbNum & 0xFF
+  ];
+  return colors[0] * 0.299 + colors[1] * 0.587 + colors[2] * 0.114;
+}
 export default function IdLabel({
   id,
   className = "",
@@ -10,14 +19,19 @@ export default function IdLabel({
   children?: string | string[];
 }) {
   const shortId = shortenAddress(id);
+  const backgroundColor = backgroundColorAddress(shortId);
   if (!children) {
     children = shortId;
   }
 
   return (
     <span
-      style={{ backgroundColor: backgroundColorAddress(shortId) }}
-      className={[className, LABEL_CLASSNAME].join(" ")}
+      style={{ backgroundColor: backgroundColor }}
+      className={[
+        className,
+        LABEL_CLASSNAME,
+        luma(backgroundColor) > 125 ? "text-black" : "text-white"
+      ].join(" ")}
       // eslint-disable-next-line
     >
       {children}
@@ -26,4 +40,4 @@ export default function IdLabel({
 }
 
 export const LABEL_CLASSNAME =
-  "font-mono text-readable-anywhere pt-0.5 px-0.5 mx-0.5 rounded opacity-75 hover:opacity-100 text-xs whitespace-nowrap";
+  "font-mono pt-0.5 px-0.5 mx-0.5 rounded opacity-75 hover:opacity-100 text-xs whitespace-nowrap";
