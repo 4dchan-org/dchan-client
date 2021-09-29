3ed4b6700bcc4b7a54ad2a8453d635d6c685e0d5
op <op@dchan.network>
2021-09-29 14:39:01 +0200

    Backlink wrap

--- dapp/src/components/post/PostHeader.tsx
+++ dapp/src/components/post/PostHeader.tsx
@@ -229,7 +229,7 @@ export default function PostHeader({
         </Link>
       </span>
       {children}
-      <span className="dchan-backlinks text-left text-sm">
+      <span className="dchan-backlinks text-left text-sm flex flex-wrap">
         {postBacklinks?.map((post) => (
           <a
             className="text-blue-600 visited:text-purple-600 hover:text-blue-500 px-1"
