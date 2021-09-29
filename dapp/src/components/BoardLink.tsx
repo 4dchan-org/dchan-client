c0819eba7d2e42a9f025cf9f5ea4323a98fede1b
Frawst <fraw@fraw.st>
2021-09-30 00:38:57 +0100

    Add time travel block to board links

--- dapp/src/components/BoardLink.tsx
+++ dapp/src/components/BoardLink.tsx
@@ -1,13 +1,15 @@
 import { Board } from "dchan";
+import useBlockNumber from "hooks/useBlockNumber";
 import { Link } from "react-router-dom";
 import { Router } from "router";
 import IdLabel from "./IdLabel";
 
 export default function BoardLink({ board }: { board: Board }) {
+  const block = useBlockNumber();
   return (
     <Link
       title={board.title}
-      to={`${Router.board(board)}`}
+      to={`${Router.board(board)}${block ? `?block=${block}` : ""}`}
     >
       <IdLabel id={board.id}>/{board.name}/</IdLabel>
     </Link>
