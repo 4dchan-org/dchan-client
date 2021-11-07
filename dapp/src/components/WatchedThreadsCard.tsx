489e08c69867eba33ca6d7e3e52718706a994d02
Frawst <fraw@fraw.st>
2021-11-07 00:35:29 +0000

    Initial shit on homepage

--- dapp/src/components/WatchedThreadsCard.tsx
+++ dapp/src/components/WatchedThreadsCard.tsx
@@ -22,10 +22,10 @@ export default function WatchedThreadsCard() {
   });
   const threads = data?.threads
   return loading || (threads && threads.length > 0) ?
-    <Card className="w-100vw max-w-initial p-2 pt-4" title={<span>👁 Watched threads</span>}>
+    <Card className="max-w-initial p-2 pt-4" title={<span>👁 Watched threads</span>}>
       {loading ? <Loading /> : <CatalogView threads={threads || []} showBoard={true} />}
     </Card>
     : (
-      <span />
+      null
     );
 }
--- dapp/src/pages/home.tsx
+++ dapp/src/pages/home.tsx
@@ -24,61 +24,67 @@ export default function HomePage({ location }: any) {
   }, [])
 
   return (
-    <div className="center-grid max-w-full min-h-screen bg-primary pt-8">
+    <div className="center-grid max-w-full min-h-screen bg-primary pt-8 flex flex-col">
       <HeaderNavigation
         baseUrl="/"
         block={`${query.block}`}
         dateTime={dateTime}
       />
-      <div className="flex flex-wrap center">
-        <Card
-          title={
-            <a className="color-black" href="/">
-              dchan.network
-            </a>
-          }
-        >
-          <div>
-            <div className="p-4 center">
+      <div className="h-full w-screen flex flex-col lg:flex-row">
+        <div className="lg:h-full lg:w-1/2 lg:float-left">
+          <div className="center">
+            <div className="mb-7">
               <img
-                className="animation-spin p-2 w-auto pointer-events-none"
+                className="animation-spin w-48 pointer-events-none mx-auto"
                 src={logo}
                 alt="dchan"
               />
+              <div className="font-mono text-4xl">dchan.network</div>
             </div>
-            <div className="p-1 text-center">
-              <small>
-                Use with{" "}
-                <a
-                  className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
-                  href="//metamask.io"
-                >
-                  Metamask
-                </a>{" "}
-                (Desktop) or{" "}
-                <a
-                  className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
-                  href="//trustwallet.com/"
-                >
-                  Trust Wallet
-                </a>{" "}
-                (Mobile)
-              </small>
+            <div className="px-8 mx-auto">
+              Welcome to dchan.network, a decentralized, time-traveling imageboard made entirely
+              with The Graph (ticker: GRT) and Polygon.
+              <br/>
+              Use with{" "}
+              <a
+                className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
+                href="//metamask.io"
+              >
+                Metamask
+              </a>{" "}
+              (Desktop) or{" "}
+              <a
+                className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
+                href="//trustwallet.com/"
+              >
+                Trust Wallet
+              </a>{" "}
+              (Mobile).
+              <br/>
+              <span className="font-bold text-contrast">ADD BETTER BLURB TEXT HERE</span>
             </div>
           </div>
-        </Card>
-
-        <div className="grid center">
-          <PopularBoardsCard />
+        </div>
+        <div className="lg:h-full lg:w-1/2 lg:float-left">
+          <article className="max-h-full max-w-100vw mr-3">
+            <header className="bg-white border border-black flex flex-row justify-evenly">
+              <div className="bg-highlight opacity-80 hover:opacity-100 py-1 px-4 w-full font-bold">Popular Boards</div>
+              <div className="border-l border-black"/>
+              <div className="bg-highlight opacity-80 hover:opacity-100 py-1 px-4 w-full font-bold">Watched Threads</div>
+              <div className="border-l border-black"/>
+              <div className="bg-highlight opacity-80 hover:opacity-100 py-1 px-4 w-full font-bold">Popular Threads</div>
+              <div className="border-l border-black"/>
+              <div className="bg-highlight opacity-80 hover:opacity-100 py-1 px-4 w-full font-bold">Latest Posts</div>
+            </header>
+            <section className={`bg-white border border-black border-t-0 w-full p-4`} style={{flex: "1 1 auto"}}>
+              <PopularBoardsCard />
+              <WatchedThreadsCard />
+              <PopularThreadsCard /> 
+              <LatestPostsCard />
+            </section>
+          </article>
         </div>
       </div>
-        
-      <WatchedThreadsCard />
-
-      <PopularThreadsCard />
-      
-      <LatestPostsCard />
-
       <Footer />
     </div>
   );
