0dcf8e892ae2983f941b660c42c0a17a1c7484d1
Frawst <fraw@fraw.st>
2021-11-12 02:37:51 +0000

    Popular threads now works properly in time travel

--- dapp/src/components/PopularThreadsCard.tsx
+++ dapp/src/components/PopularThreadsCard.tsx
@@ -4,11 +4,16 @@ import THREADS_LIST_MOST_POPULAR from "graphql/queries/threads/list_most_popular
 import THREADS_LIST_MOST_POPULAR_BLOCK from "graphql/queries/threads/list_most_popular_block";
 import CatalogView from "./CatalogView";
 import Loading from "./Loading";
+import { useTraveledBlock } from "./TimeTravelWidget";
 import { DateTime } from "luxon";
 import { useMemo } from "react";
 
 export default function PopularThreadsCard({block} : {block?: number}) {
-  const cutoff = useMemo(() => Math.floor(DateTime.now().toSeconds()-604800), [])
+  const currentBlock = useTraveledBlock();
+  console.log(`currentBlock is ${JSON.stringify(currentBlock)}`);
+  const timestamp = currentBlock ? parseInt(currentBlock.timestamp) : DateTime.now().toSeconds();
+  console.log(`timestamp is ${timestamp}`);
+  const cutoff = useMemo(() => Math.floor(timestamp-604800), [currentBlock])
 
   const query = block ? THREADS_LIST_MOST_POPULAR_BLOCK : THREADS_LIST_MOST_POPULAR;
   const { loading, data } = useQuery<{ threads: Thread[] }, any>(query, {
@@ -16,7 +21,7 @@ export default function PopularThreadsCard({block} : {block?: number}) {
     fetchPolicy: block ? "cache-first" : "network-only",
     variables: {
       cutoff,
-      block,
+      block: currentBlock ? parseInt(currentBlock.number) : block,
     },
   });
 
