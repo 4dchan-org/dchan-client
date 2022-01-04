67b5a04518e561a0355ecb16a13d4825251fc444
Frawst <fraw@fraw.st>
2022-01-04 17:16:29 +0000

    Redo homepage again

--- dapp/src/pages/home.tsx
+++ dapp/src/pages/home.tsx
@@ -1,16 +1,14 @@
-import logo from "assets/images/dchan.png";
-import TabbedCard from "components/TabbedCard";
+import Card from "components/Card";
 import Footer from "components/Footer";
 import HeaderNavigation from "components/header/HeaderNavigation";
 import LatestPostsCard from "components/LatestPostsCard";
-import PopularBoardsCard from "components/PopularBoardsCard";
 import PopularThreadsCard from "components/PopularThreadsCard";
 import WatchedThreadsCard from "components/WatchedThreadsCard";
 import { parse as parseQueryString } from "query-string";
 import { DateTime } from "luxon";
 import { useEffect } from "react";
 import { useTitle } from "react-use";
-import { Polygon, TheGraph } from "components/FAQCard";
+import HeaderLogo from "components/header/logo";
 
 export default function HomePage({ location }: any) {
   useTitle("dchan.network");
@@ -33,49 +31,19 @@ export default function HomePage({ location }: any) {
         block={queriedBlock ? `${queriedBlock}` : undefined}
         dateTime={dateTime}
       />
-      <div className="lg:h-full max-w-full flex flex-col lg:flex-row pt-8" style={{flex: "1 0 auto", paddingBottom: "68.8px"}}>
-        <div className="lg:my-auto lg:w-1/2 lg:float-left px-8 pb-8">
-          <div className="mb-7">
-            <img
-              className="animation-spin w-48 pointer-events-none mx-auto"
-              src={logo}
-              alt="dchan"
-            />
-            <div className="font-mono text-4xl">dchan.network</div>
-          </div>
-          <div className="px-8 mx-auto">
-            Welcome to dchan.network, a decentralized time-traveling imageboard made entirely
-            using <TheGraph/> and <Polygon/>.
-            <br/>
-            Use with{" "}
-            <a
-              className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
-              href="//metamask.io"
-            >
-              Metamask
-            </a>{" "}
-            (Desktop) or{" "}
-            <a
-              className="text-blue-600 visited:text-purple-600 hover:text-blue-500"
-              href="//trustwallet.com/"
-            >
-              Trust Wallet
-            </a>{" "}
-            (Mobile).
-          </div>
-        </div>
-        <div className="lg:h-full lg:w-1/2 lg:float-left flex flex-col lg:px-8 lg:pb-6">
-          <TabbedCard className="h-full flex flex-col" containerClassName="my-auto">
-            {new Map([
-              ["Popular Boards", <PopularBoardsCard block={queriedBlock} />],
-              ["Watched Threads", <WatchedThreadsCard block={queriedBlock} />],
-              ["Popular Threads", <PopularThreadsCard block={queriedBlock} />],
-              ["Latest Posts", <LatestPostsCard block={queriedBlock} />],
-            ])}
-          </TabbedCard>
-        </div>
+      <HeaderLogo/>
+      <div className="flex flex-row px-4">
+        <Card title={<span>Watched Threads</span>} className="w-1/2 pr-2 pb-4">
+          <WatchedThreadsCard block={queriedBlock} />
+        </Card>
+        <Card title={<span>Popular Threads</span>} className="w-1/2 pl-2 pb-4">
+          <PopularThreadsCard block={queriedBlock} />
+        </Card>
       </div>
-      <Footer className="mt-auto lg:mt-0 lg:fixed lg:bottom-0 lg:left-0 lg:right-0"/>
+      <Card title={<span>Latest Posts</span>} className="w-full px-4 pb-4">
+        <LatestPostsCard block={queriedBlock} />
+      </Card>
+      <Footer/>
     </div>
   );
 }
