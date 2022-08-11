a65f67f55e81002e5658cc5cb9f41e3f6a41977c
op <op@dchan.network>
2022-08-11 10:59:20 +0200

    Fix NSFW boards showing all images as NSFW

--- dapp/src/components/CatalogView.tsx
+++ dapp/src/components/CatalogView.tsx
@@ -1,12 +1,14 @@
-import { Thread } from "dchan";
+import { Board, Thread } from "dchan";
 import CatalogThread from "./catalog/CatalogThread";
 
 export default function CatalogView({
   threads,
+  board,
   block,
   showBoard = false
 }: {
   threads: Thread[];
+  board?: Board,
   block?: number,
   showBoard?: boolean
 }) {
@@ -14,6 +16,7 @@ export default function CatalogView({
     <div className="flex flex-row flex-wrap justify-center place-items-start font-size-090rem px-2 sm:px-4 flex-grow">
       {threads.filter(t => !!t && !!t.board).map(thread => (
         <CatalogThread
+          board={board}
           thread={thread}
           key={thread.id}
           block={block}
