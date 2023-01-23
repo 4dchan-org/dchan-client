f21440d129d59a8bb24c40577192045fe11fac1e
op <op@dchan.network>
2023-01-24 00:54:39 +0100

    Disabled posting during time travel

--- src/dchan/components/ContentHeader.tsx
+++ src/dchan/components/ContentHeader.tsx
@@ -1,8 +1,15 @@
 import { useThrottleCallback } from "@react-hook/throttle";
 import { Board, Thread } from "dchan/subgraph/types";
 import { ReactElement } from "react";
-import { BoardHeader, FormPost, RefreshWidget, BoardViewSettings, ContentNavigation } from ".";
+import {
+  BoardHeader,
+  FormPost,
+  RefreshWidget,
+  BoardViewSettings,
+  ContentNavigation,
+} from ".";
 import useTimeTravel from "dchan/hooks/useTimeTravel";
+import { DateTime } from "luxon";
 
 export const ContentHeader = ({
   board,
@@ -17,30 +24,37 @@ export const ContentHeader = ({
   summary?: ReactElement;
   onRefresh: () => void;
 }) => {
-  const { timeTraveledToBlockNumber: block, lastBlock } = useTimeTravel()
+  const {
+    timeTraveledToBlockNumber: block,
+    timeTraveledToDateTime,
+    lastBlock,
+    isTimeTraveling,
+  } = useTimeTravel();
   const throttledRefresh = useThrottleCallback(onRefresh, 1, true);
 
   return (
     <div>
-      <BoardHeader
-        title={title}
-        board={board}
-        thread={thread}
-      />
+      <BoardHeader title={title} board={board} thread={thread} />
 
-      {board === null
-       ? ""
-       : (thread || board)
-       ? <FormPost thread={thread} board={board} />
-       : "..."}
+      {board === null ? (
+        ""
+      ) : thread || board ? (
+        isTimeTraveling ? (
+          <span className="opacity-60">Time traveled to {timeTraveledToDateTime?.toLocaleString(DateTime.DATETIME_SHORT)}</span>
+        ) : (
+          <FormPost thread={thread} board={board} />
+        )
+      ) : (
+        "..."
+      )}
 
-      {board !== null ?
+      {board !== null ? (
         <div className="p-2">
           <hr></hr>
         </div>
-      :
+      ) : (
         ""
-      }
+      )}
 
       <div className="text-center sm:text-left grid xl:grid-cols-3 text-xs">
         <div className="mx-2 flex flex-wrap sm:flex-nowrap justify-center md:justify-start items-center">
@@ -53,9 +67,15 @@ export const ContentHeader = ({
           )}
         </div>
         <div className="center grid">
-          {summary ? <span className="py-2 text-xs text-gray-600">{summary}</span> : <span/>}
+          {summary ? (
+            <span className="py-2 text-xs text-gray-600">{summary}</span>
+          ) : (
+            <span />
+          )}
         </div>
-        <div className="flex flex-wrap justify-center md:justify-end items-center pr-2">{!thread && !!board ? <BoardViewSettings /> : ""}</div>
+        <div className="flex flex-wrap justify-center md:justify-end items-center pr-2">
+          {!thread && !!board ? <BoardViewSettings /> : ""}
+        </div>
       </div>
 
       <div className="p-2">
@@ -63,4 +83,4 @@ export const ContentHeader = ({
       </div>
     </div>
   );
-}
+};
