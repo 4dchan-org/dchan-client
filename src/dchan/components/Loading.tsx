5fe6cc880aab59aafa3121c981a9f1d52a6bf0bc
op <op@dchan.network>
2023-01-25 11:18:27 +0100

    Time traveling loader

--- src/dchan/components/Loading.tsx
+++ src/dchan/components/Loading.tsx
@@ -1,19 +1,30 @@
 import { useTimeTravel } from "dchan/hooks";
+import { DateTime } from "luxon";
 import { Spinner } from ".";
 
-export const Loading = ({
-  className = ""
-}: {
-  className?: string
-}) => {
-    const { isTimeTraveling } = useTimeTravel()
+export const Loading = ({ className = "" }: { className?: string }) => {
+  const { isTimeTraveling, timeTraveledToDateTime } = useTimeTravel();
 
-    return (
-      <div className={`${className} dchan-loading flex center p-2`}>
-          <div className="opacity-80 pr-1"><Spinner size={8} reverse={isTimeTraveling} speed={"sanic"} /></div>
-          <div className="">
-            {isTimeTraveling ? "Rewinding" : "Loading..."}
-          </div>
+  return (
+    <div className={`${className} dchan-loading flex center p-2`}>
+      <div className="opacity-80 pr-1">
+        <Spinner size={8} reverse={isTimeTraveling} speed={"sanic"} />
       </div>
-    )
-  }
+      <div className="">
+        {isTimeTraveling ? (
+          <>
+            <div>Time traveling to...</div>
+            <div className="text-xs opacity-70">
+              {timeTraveledToDateTime?.toLocaleString(DateTime.DATETIME_SHORT)}
+            </div>
+            <div className="text-xs opacity-50">
+              {timeTraveledToDateTime?.toRelative()}
+            </div>
+          </>
+        ) : (
+          <>Loading...</>
+        )}
+      </div>
+    </div>
+  );
+};
